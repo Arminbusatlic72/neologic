4e4af5aa8f9ec1f29cbb99ad718d079672bc2ef4
Armin Busatlic <dzarma.busatlic@gmail.com>
2020-03-15 22:48:45 +0100

    Adding Project Carousel and modal

---
+++ src/components/portfolio/ProjectCarousel.js
@@ -0,0 +1,34 @@
+import React from "react"
+import PropTypes from "prop-types"
+import { Carousel } from "react-bootstrap"
+import Img from "gatsby-image"
+
+import "./carousel.scss"
+
+const ProjectCarousel = ({ images, current }) => {
+    const carouselItems = images.map((image, index) =>
+        <Carousel.Item key={index}>
+            <figure>
+                <img src={image.src} alt={image.alt} />
+                <figcaption>
+                    <div className="bottom-bar">
+                        <div className="counter">{index + 1} of {images.length}</div>
+                    </div>
+                </figcaption>
+            </figure>
+        </Carousel.Item>
+    );
+
+    return (
+        <Carousel interval={null} indicators={false} defaultActiveIndex={current} slide={false}>
+            {carouselItems}
+        </Carousel>
+    );
+}
+
+ProjectCarousel.propTypes = {
+    images: PropTypes.array.isRequired,
+    current: PropTypes.number.isRequired,
+}
+
+export default ProjectCarousel;
--- src/templates/projekat.js
+++ src/templates/projekat.js
@@ -3,8 +3,9 @@ import { graphql } from 'gatsby'
 import Layout from '../components/layout'
 import SEO from '../components/seo'
 // import Img from 'gatsby-image'
+
 import PortfolioModal from "../components/portfolio/modal"
-import PortfolioCarousel from "../components/portfolio/carousel"
+import ProjectCarousel from '../components/portfolio/ProjectCarousel'
 
 export const query = graphql`
 query($slug: String!) {
@@ -53,69 +54,100 @@ query($slug: String!) {
 
 `
 
-const Project = (props) => {
+class Project extends React.Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      modalShow: false,
+      modalCurrent: 0
+    }
+    this.handlePortfolioClick = this.handlePortfolioClick.bind(this);
+    this.setModal = this.setModal.bind(this);
+
+    this.src = this.props.data.wpgraphql.projectBy.featuredImage.sourceUrl;
+    this.alt = this.props.data.wpgraphql.projectBy.featuredImage.altText;
+    this.src1 = this.props.data.wpgraphql.projectBy.ProjectDetails.image.sourceUrl;
+    this.alt1 = this.props.data.wpgraphql.projectBy.ProjectDetails.image.altText;
+    this.src2 = this.props.data.wpgraphql.projectBy.ProjectDetails.image1.sourceUrl;
+    this.alt2 = this.props.data.wpgraphql.projectBy.ProjectDetails.image1.altText;
+    this.src3 = this.props.data.wpgraphql.projectBy.ProjectDetails.image2.sourceUrl;
+    this.alt3 = this.props.data.wpgraphql.projectBy.ProjectDetails.image2.altText;
+
+    this.projectImages = [
+      { src: this.src, alt: this.alt },
+      { src: this.src1, alt: this.alt1 },
+      { src: this.src, alt: this.alt }
+    ]
+  }
+
+  handlePortfolioClick(index, e) {
+    e.preventDefault();
+    this.setModal(true, index);
+  }
+
+  setModal(isShown, current) {
+    this.setState({
+      modalShow: isShown,
+      modalCurrent: current
+    });
+  }
+
+  render() {
+
+    return (
+
+      <Layout>
+        < SEO title="/project" />
+        <header className="project__bcg">
+          <img className="header__bcg" src={this.src} alt={this.alt} />
 
 
+          <div className="container h-100">
 
+            <div className="row h-100 align-items-center justify-content-center text-center">
+              <div className="col-lg-10 align-self-end">
+                <h2 className="fade-in text-uppercase text-white font-weight-bold">{this.props.data.wpgraphql.projectBy.title}</h2>
+                <hr className="divider my-4" />
+              </div>
 
-  let src = props.data.wpgraphql.projectBy.featuredImage.sourceUrl;
-  let alt = props.data.wpgraphql.projectBy.featuredImage.altText;
-  let src1 = props.data.wpgraphql.projectBy.ProjectDetails.image.sourceUrl;
-  let alt1 = props.data.wpgraphql.projectBy.ProjectDetails.image.altText;
-  let src2 = props.data.wpgraphql.projectBy.ProjectDetails.image1.sourceUrl;
-  let alt2 = props.data.wpgraphql.projectBy.ProjectDetails.image1.altText;
-  let src3 = props.data.wpgraphql.projectBy.ProjectDetails.image2.sourceUrl;
-  let alt3 = props.data.wpgraphql.projectBy.ProjectDetails.image2.altText;
-
-  return (
-
-    <Layout>
-      < SEO title="/project" />
-      <header className="project__bcg">
-        <img className="header__bcg" src={src} alt={alt} />
-
-
-        <div className="container h-100">
-
-          <div className="row h-100 align-items-center justify-content-center text-center">
-            <div className="col-lg-10 align-self-end">
-              <h2 className="fade-in text-uppercase text-white font-weight-bold">{props.data.wpgraphql.projectBy.title}</h2>
-              <hr className="divider my-4" />
             </div>
+          </div>
+        </header>
+        <section className="page-section">
+          <div className="container">
+            <div className="row">
+              <div className="col-12 col-lg-6">
+                <ul className="d-flex flex-column">
+                  <li><p>Lokacija: {this.props.data.wpgraphql.projectBy.ProjectDetails.lokacija}</p></li>
+                  <li><p>Godina: {this.props.data.wpgraphql.projectBy.ProjectDetails.godina}</p></li>
+                  <li><p>Enterijer: {this.props.data.wpgraphql.projectBy.ProjectDetails.enterijer}</p></li>
+                  <li><p>Foto: {this.props.data.wpgraphql.projectBy.ProjectDetails.foto}</p></li>
+                  <li><p>Brendovi: {this.props.data.wpgraphql.projectBy.ProjectDetails.brendovi}</p></li>
+                  <li><p>{this.props.data.wpgraphql.projectBy.ProjectDetails.projectDescription}</p></li>
+                </ul>
+              </div>
+              <div className="col-12 col-md-6 mb-5" onClick={this.handlePortfolioClick.bind(this, 0)}>
+                <img className="img-fluid" src={this.src1} alt={this.alt1} />
+              </div>
+              <div className="col-12 col-md-6 mb-5">
+                <img className="img-fluid" src={this.src2} alt={this.alt2} />
+              </div>
+              <div className="col-12 col-md-6 mb-5">
+                <img className="img-fluid" src={this.src3} alt={this.alt3} />
+              </div>
+
+            </div>
+
 
           </div>
-        </div>
-      </header>
-      <section className="page-section">
-        <div className="container">
-          <div className="row">
-            <div className="col-12 col-lg-6">
-              <ul className="d-flex flex-column">
-                <li><p>Lokacija: {props.data.wpgraphql.projectBy.ProjectDetails.lokacija}</p></li>
-                <li><p>Godina: {props.data.wpgraphql.projectBy.ProjectDetails.godina}</p></li>
-                <li><p>Enterijer: {props.data.wpgraphql.projectBy.ProjectDetails.enterijer}</p></li>
-                <li><p>Foto: {props.data.wpgraphql.projectBy.ProjectDetails.foto}</p></li>
-                <li><p>Brendovi: {props.data.wpgraphql.projectBy.ProjectDetails.brendovi}</p></li>
-                <li><p>{props.data.wpgraphql.projectBy.ProjectDetails.projectDescription}</p></li>
-              </ul>
-            </div>
-            <div className="col-12 col-md-6 mb-5">
-              <img className="img-fluid" src={src1} alt={alt1} />
-            </div>
-            <div className="col-12 col-md-6 mb-5">
-              <img className="img-fluid" src={src2} alt={alt2} />
-            </div>
-            <div className="col-12 col-md-6 mb-5">
-              <img className="img-fluid" src={src3} alt={alt3} />
-            </div>
-
-          </div>
-
-
-        </div>
-      </section>
-    </Layout>
-  )
+        </section>
+        <PortfolioModal show={this.state.modalShow} onHide={() => this.setModal(false, 0)}>
+          <ProjectCarousel images={this.projectImages} current={this.state.modalCurrent} />
+        </PortfolioModal>
+      </Layout>
+    )
+  }
 }
 
-export default Project
+export default Project;
