d4769b685cb1f221ed3ac59618b00e89daf35c93
Armin Busatlic <dzarma.busatlic@gmail.com>
2020-04-29 01:08:22 +0200

    Adding hidden input fields to contact form

--- src/components/contact-form.js
+++ src/components/contact-form.js
@@ -15,6 +15,9 @@ const ContactForm = () => {
                 method="post"
                 data-netlify="true"
                 data-netlify-honeypot="bot-field">
+                <input type="hidden" name="bot-field" />
+                <input type="hidden" name="form-name" value="contact" />
+
 
 
 
